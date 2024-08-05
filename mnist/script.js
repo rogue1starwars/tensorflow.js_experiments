8aa2a393857eeb20a293af578b54ac98c75a51b9
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-08-05 21:37:18 +0900

    initial commit

---
+++ mnist/script.js
@@ -0,0 +1,113 @@
+import { MnistData } from "./data.js";
+
+async function showExamples(data) {
+  // Create a container in the visor
+  const surface = tfvis
+    .visor()
+    .surface({ name: "Input Data Examples", tab: "Input Data" });
+
+  // Get the examples
+  const examples = data.nextTestBatch(20);
+  const numExamples = examples.xs.shape[0];
+
+  // Create a canvas element to render each example
+  for (let i = 0; i < numExamples; i++) {
+    const imageTensor = tf.tidy(() => {
+      // Reshape the image to 28x28 px
+      return examples.xs
+        .slice([i, 0], [1, examples.xs.shape[1]])
+        .reshape([28, 28, 1]);
+    });
+
+    const canvas = document.createElement("canvas");
+    canvas.width = 28;
+    canvas.height = 28;
+    canvas.style = "margin: 4px;";
+    await tf.browser.toPixels(imageTensor, canvas);
+    surface.drawArea.appendChild(canvas);
+
+    imageTensor.dispose();
+  }
+}
+
+async function run() {
+  const data = new MnistData();
+  await data.load();
+  await showExamples(data);
+}
+
+document.addEventListener("DOMContentLoaded", run);
+
+function getModel() {
+  const model = tf.sequential();
+
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const IMAGE_CHANNELS = 1;
+
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+      kernelSize: 5,
+      filters: 8,
+      strides: 1,
+      activation: "relu",
+      kernelInitializer: "VarianceScaling",
+    }),
+  );
+
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+      kernelSize: 5,
+      filters: 8,
+      strides: 1,
+      activation: "relu",
+      kernelInitializer: "VarianceScaling",
+    }),
+  );
+
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+
+  model.add(tf.layers.flatten());
+
+  const NUM_OUTPUT_CLASSES = 10;
+  model.add(
+    tf.layers.dense({
+      units: NUM_OUTPUT_CLASSES,
+      kernelInitializer: "VarianceScaling",
+      activation: "softmax",
+    }),
+  );
+
+  const optimizer = tf.train.adam();
+  model.compile({
+    optimizer: optimizer,
+    loss: "categoricalCrossentropy",
+    metrics: ["accuracy"],
+  });
+
+  return model;
+}
+
+async function train(model, data) {
+  const metrics = ["loss", "val_loss", "acc", "val_acc"];
+  const container = {
+    name: "Model Training",
+    tab: "Model",
+    styles: { height: "1000px" },
+  };
+
+  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+
+  const BATCH_SIZE = 512;
+  const TRAIN_DATA_SIZE = 5500;
+  const TEST_DATA_SIZE = 1000;
+
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+    return [d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]), d.labels];
+  });
+}
